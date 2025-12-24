682de477bf12655778d205a2ae689f2909549abd
unknown <pk123456434@gmail.com>
2025-12-24 21:00:28 +0545

    Update practicenp.py and other files

--- practicenp.py
+++ practicenp.py
@@ -2,11 +2,28 @@ import numpy as np
 
 #slicing
 
-array = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+"""array = np.array([
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
+])"""
 
 # array[start:end:step]
 
-print(array[0:2])
+#print(array[-4:-1])
 
-
+#print(array[::-1])
 #12 min to bro code
+
+#arithmetic operations]]
+
+#scalar Arithmetic]]
+
+array =np.array([1,2,3,4])
+print(array +1)
+print(array -2)
+print(array *3)
+print(array /4)
+
+#stop at 25 min
